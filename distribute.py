8ba11bc4d21ff5f7de30d4af854283938621b3e3
Matthias Kubisch <kubisch@informatik.hu-berlin.de>
2016-10-08 00:10:55 +0200

    First version of distribution script.

---
+++ distribute.py
@@ -0,0 +1,87 @@
+#!/usr/bin/python
+
+# you may need to install these packages on your machine
+# sudo pip install paramiko 
+# sudo apt-get install libffi6, libffi-dev, python2.7-dev, libssl-dev
+# sudo pip install cryptography --force-reinstall
+
+import re, sys, shlex, argparse, getpass, paramiko, time
+from subprocess import Popen, PIPE
+from os import listdir
+from os.path import isfile, isdir, join
+
+robot_list  = ["crawler", "tadpole", "fourlegged", "humanoid", "_scrtst"]
+data_path   = "../data/exp/"
+
+host_domain = ".informatik.hu-berlin.de"
+server_list = [ "gruenau1"
+              , "gruenau2"
+              , "gruenau3"
+              , "gruenau4" 
+              , "gruenau5"
+              , "gruenau6"
+              , "gruenau7"
+              , "gruenau8"
+              ]
+
+execute_commands = ["echo 'cd work/diss/evolution; nohup ./evolution.py -r {0} -n {1} -x &' > run.sh", "bash run.sh </dev/null >&/dev/null" ]
+
+def start_experiment(server, robot, num, user, pswd):
+    print("Connecting to: {0}".format(server)),
+
+    try:
+        ssh = paramiko.SSHClient()
+        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        ssh.connect(server, username=user, password=pswd)
+        print("SUCCESS."),
+        for cmd in execute_commands:        
+            stdin, stdout, stderr = ssh.exec_command(cmd.format(robot, num))
+            lines = stdout.readlines() + stderr.readlines()
+            for line in lines:
+                print(line)
+        
+        ssh.close()
+        print("DONE.")
+        time.sleep(3)
+    except:
+        print("FAILED.")
+
+
+def start_all(server_list, robot, num, user, pswd):
+    for server in server_list:
+        start_experiment(server+host_domain, robot, num, user, pswd)
+
+
+def main():
+    global port_start, num_conductions
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-r', '--robot' , default = "")
+    parser.add_argument('-n', '--number', default = 0 )
+    args = parser.parse_args()
+
+    robot = str(args.robot)
+    if robot == "":
+        print("No robot defined")
+        return 
+
+    num_exp = int(args.number)
+    if num_exp == 0:
+        print("Number of experiments not specified.")
+        return
+
+    print("selected robot is: {0}".format(robot))
+    print("number of experiments: {0}.".format(num_exp))
+
+    user = raw_input("Username: ")
+    if user == "":
+        print("Aborted.")
+        return
+
+    pswd = getpass.getpass()
+
+    start_all(server_list, robot, num_exp, user, pswd)
+    print("\n____\nDONE distributing.")
+
+if __name__ == "__main__": main()
+
