4ccf382a1c88574d69a55d002035a070b2d90573
Matthias Kubisch <kubisch@informatik.hu-berlin.de>
2018-11-08 11:54:22 +0100

    remove jpgs also before running the videoscript.

--- ppm2avi.sh
+++ ppm2avi.sh
@@ -1,4 +1,5 @@
 cd ./data
+rm *.jpg
 for f in *.ppm; do convert -quality 100 $f `basename $f .ppm`.jpg; rm $f; done
 #mencoder mf://*.png -mf fps=25:type=png -ovc lavc -lavcopts vcodec=mpeg4:mbd=2:trell -oac copy -o output.avi
 #mencoder mf://*.jpg -mf fps=25:type=jpg -ovc lavc -lavcopts vcodec=mpeg4:mbd=2:mv0:trell:v4mv:cbp:last_pred=3:predia=2:dia=2:vmax_b_frames=2:vb_strategy=1:precmp=2:cmp=2:subcmp=2:preme=2:qns=2 -oac copy -o $1.avi
